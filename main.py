66d856bd41d024ecd431f9b1a6c3ff112b88532e
[Students] Edzus Kivilands <81DEKivilands@rvt.lv>
2020-02-17 14:05:00 +0200

    js

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from file_proc import read_file, write_file
 
 app = Flask(__name__)
 
@@ -25,5 +26,29 @@ def params():
 @app.route("/post_req", methods = ["POST"])
 def post_req():
   return request.args
+
+@app.route("/read_file")
+def read_from_file():
+   content = read_file()
+   return content
+
+@app.route("/write_file", methods = ["POST"])
+def write_to_file():
+  if request.content_type == "application/json":
+    contentJSON = request.get_json()
+    write_file(contentJSON["data"])
+    return F"Add line {contentJSON['data']} to file."
+  else:
+    return F"Invalid request {request.content_type} not supported"
+
+@app.route("/file", methods = ["GET", "POST"])
+def filework():
+  if request.method == "GET":
+    return read_from_file()
+  elif request.method == "POST":
+    return write_to_file()
+  else:
+    return f"Method is {request.method} not supported"
+  
 
 app.run(host="0.0.0.0", port=8020, threaded=True,debug=True)
